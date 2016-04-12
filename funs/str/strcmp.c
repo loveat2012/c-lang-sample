3d98b6081fbb597503ab07415b0acb4d6d7a5955
loveat2012 <xianmin2004@163.com>
2016-04-12 17:21:11 +0800

    add strcmp function sample file

--- funs/str/strcmp.c
+++ funs/str/strcmp.c
@@ -1,4 +1,14 @@
-//
-// Created by dev on 16/4/12.
-//
+#include <stdio.h>
+#include <string.h>
 
+int main(int argc, char *args[])
+{
+    const char *cmpstr =  "a";
+    const char *becmpdstr = "auto";
+    if (strcmp(cmpstr, becmpdstr)) {
+        printf("%s is in %s!\n", cmpstr, becmpdstr);
+    } else {
+        printf("%s is not  in %s!\n", cmpstr, becmpdstr);
+    }
+    return 0;
+}
