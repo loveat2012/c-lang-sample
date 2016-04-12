6358fd9e43b6b56923e0957e36c05039294a1217
loveat2012 <xianmin2004@163.com>
2016-04-12 18:33:42 +0800

    modfied strcmp sample

--- funs/str/strcmp.c
+++ funs/str/strcmp.c
@@ -3,12 +3,25 @@
 
 int main(int argc, char *args[])
 {
-    const char *cmpstr =  "a";
+    const char *cmpstr1 =  "auto";
+    const char *cmpstr2 =  "au";
+    const char *cmpstr3 =  "u";
     const char *becmpdstr = "auto";
-    if (strcmp(cmpstr, becmpdstr)) {
-        printf("%s is in %s!\n", cmpstr, becmpdstr);
+    int pos;
+    if (strcmp(cmpstr1, becmpdstr) == 0) {
+        printf("%s is in %s!\n", cmpstr1, becmpdstr);
     } else {
-        printf("%s is not  in %s!\n", cmpstr, becmpdstr);
+        printf("%s is not  in %s!\n", cmpstr1, becmpdstr);
+    }
+    if ((pos = strcmp(cmpstr2, becmpdstr))) {
+        printf("%s is in %s, pos: %d\n", cmpstr2, becmpdstr, pos);
+    } else {
+        printf("%s is not  in %s!\n", cmpstr2, becmpdstr);
+    }
+    if (strcmp(cmpstr3, becmpdstr) == 0) {
+        printf("%s is in %s!\n", cmpstr3, becmpdstr);
+    } else {
+        printf("%s is not  in %s!\n", cmpstr3, becmpdstr);
     }
     return 0;
 }
